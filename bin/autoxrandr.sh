efba7b42924d2c565abe06a49ded9e3f2ef21529
lebenlechzer <lebenlechzer@gmail.com>
2017-03-12 22:56:55 +0100

    Better monitor handling

--- bin/autoxrandr.sh
+++ bin/autoxrandr.sh
@@ -7,21 +7,26 @@ export XAUTHORITY=/home/wolfgang/.Xauthority
 
 function connect(){
    logger -t udev "$1 connected"
-   xrandr --output $1 --auto --right-of LVDS1
+   xrandr --output $1 --primary --auto --right-of LVDS1
+   echo "connect $(date)" >> /home/wolfgang/log
 }
 
 function disconnect(){
    logger -t udev "$1 disconnected"
    xrandr --output $1 --off
+   xrandr --output LVDS1 --primary
+   echo "disconnect $(date)" >> /home/wolfgang/log
 }
 
 query=$(xrandr --query)
 connected_output=$(echo "${query}" | egrep " connected \(" | awk -F' ' '{print $1}')
-if [[ $? == 0 ]]; then
+echo "co $connected_output"
+if [[ ! -z "${connected_output// }" ]]; then
    connect $connected_output
 else
-   disconnected_output=$(echo "${query}" | egrep " disconnected [0-9]+x[0-9]+\+[0-9]+\+[0-9]+" | awk -F' ' '{print $1}')
-   if [[ $? == 0 ]]; then
+   disconnected_output=$(echo "${query}" | egrep " disconnected [^(]" | awk -F' ' '{print $1}')
+   echo "do $disconnected_output"
+   if [[ ! -z "${disconnected_output// }"  ]]; then
       disconnect $disconnected_output
    fi
 fi
